432421a7554c1ae4b5d98053c0e619a64d73c6e6
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-02-05 14:01:57 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,174 @@
+import { useState, useEffect } from 'react';
+import { NFTStorage, File } from 'nft.storage'
+import { Buffer } from 'buffer';
+import { ethers } from 'ethers';
+import axios from 'axios';
+
+// Components
+import Spinner from 'react-bootstrap/Spinner';
+import Navigation from './components/Navigation';
+
+// ABIs
+import NFT from './abis/NFT.json'
+
+// Config
+import config from './config.json';
+
+function App() {
+  const [provider, setProvider] = useState(null)
+  const [account, setAccount] = useState(null)
+  const [nft, setNFT] = useState(null)
+
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
+  const [image, setImage] = useState(null)
+  const [url, setURL] = useState(null)
+
+  const [message, setMessage] = useState("")
+  const [isWaiting, setIsWaiting] = useState(false)
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+
+    const nft = new ethers.Contract(config[network.chainId].nft.address, NFT, provider)
+    setNFT(nft)
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+
+    if (name === "" || description === "") {
+      window.alert("Provide a name and description")
+      return
+    }
+
+    setIsWaiting(true)
+
+    //AI API to generate a image
+    const imageData = await createImage()
+
+    //IPFS (NFT.Storage)
+    const url = await uploadImage(imageData)
+
+    // Mint
+    await mintImage(url)
+
+    setIsWaiting(false)
+    setMessage("")
+  }
+
+  const createImage = async () => {
+    setMessage("Rendering Image...")
+
+    //AI API
+    const URL = `https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2`
+
+    // Send the request
+    const response = await axios({
+      url: URL,
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_HUGGING_FACE_API_KEY}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      data: JSON.stringify({
+        inputs: description, options: { wait_for_model: true },
+      }),
+      responseType: 'arraybuffer',
+    })
+
+    const type = response.headers['content-type']
+    const data = response.data
+
+    const base64data = Buffer.from(data).toString('base64')
+    const img = `data:${type};base64,` + base64data // <-- This is so we can render it on the page
+    setImage(img)
+
+    return data
+  }
+
+  const uploadImage = async (imageData) => {
+    setMessage("Uploading Image...")
+
+    const nftstorage = new NFTStorage({ token: process.env.REACT_APP_NFT_STORAGE_API_KEY })
+
+    // Send request to store image
+    const { ipnft } = await nftstorage.store({
+      image: new File([imageData], "image.jpeg", { type: "image/jpeg" }),
+      name: name,
+      description: description,
+    })
+
+    // IPFS URL
+    const url = `https://ipfs.io/ipfs/${ipnft}/metadata.json`
+    setURL(url)
+
+    return url
+  }
+
+  const mintImage = async (tokenURI) => {
+    setMessage("Waiting for Mint...")
+
+    const signer = await provider.getSigner()
+    const transaction = await nft.connect(signer).mint(tokenURI, { value: ethers.utils.parseUnits("1", "ether") })
+    await transaction.wait()
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
+  return (
+    <div>
+      <Navigation account={account} setAccount={setAccount} />
+
+      <div>
+            <div>
+                <a href="https://github.com/Joseph-DelGiorgio" target="_blank">
+                Github
+                </a>
+            </div>
+            <div>
+                <a href="https://twitter.com/JoeDelGiorgio1" target="_blank">
+                Twitter
+                </a>
+            </div>
+      </div>
+
+
+      <div className='form'>
+        <form onSubmit={submitHandler}>
+          <input type="text" placeholder="Name of your image" onChange={(e) => { setName(e.target.value) }} />
+          <input type="text" placeholder="description of image..." onChange={(e) => setDescription(e.target.value)} />
+          <input type="submit" value="Mint" />
+        </form>
+
+        <div className="image">
+          {!isWaiting && image ? (
+            <img src={image} alt="AI generated image" />
+          ) : isWaiting ? (
+            <div className="image__placeholder">
+              <Spinner animation="border" />
+              <p>{message}</p>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+
+      {!isWaiting && url && (
+        <p>
+          View&nbsp;<a href={url} target="_blank" rel="noreferrer">Metadata 4 Devs</a>
+        </p>
+      )}
+    </div>
+    
+  );
+}
+
+export default App;
